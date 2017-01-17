293d6a8b151e80248d8b1113ba3af9bb9fae771f
yimin-du <d.yimin@gmail.com>
2017-01-17 14:48:56 +1030

    add Movies and Customers controllers

---
+++ VideoRentalStore/Controllers/CustomersController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoRentalStore.Models;
+using VideoRentalStore.ViewModels;
+
+namespace VideoRentalStore.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            var customers = new List<Customer>
+            {
+                 new Customer
+                    {
+                        Id = 1,
+                        Name = "John Smith"
+                    },
+                    new Customer
+                    {
+                        Id = 2,
+                        Name = "Mary Williams"
+                    }
+            };
+
+            return customers;              
+        }
+    }
+}
---
+++ VideoRentalStore/Controllers/MoviesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoRentalStore.Models;
+using VideoRentalStore.ViewModels;
+
+namespace VideoRentalStore.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // /Movies
+        [Route("movies")]
+        public ActionResult Index()
+        {
+
+
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        // GET: /Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer1" },
+                new Customer { Name = "Customer2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        
+
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+             {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+             };
+        }
+    }
+}
