f9fe8bd2cc3ffa91ecfdfcb9ace5f0d426d7a3f9
yimin-du <d.yimin@gmail.com>
2017-01-18 21:24:16 +1030

    implement customer form

--- VideoRentalStore/Controllers/CustomersController.cs
+++ VideoRentalStore/Controllers/CustomersController.cs
@@ -29,6 +29,34 @@ namespace VideoRentalStore.Controllers
             return View(customers);
         }
 
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var custmerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                custmerInDb.Name = customer.Name;
+                custmerInDb.Birthday = customer.Birthday;
+                custmerInDb.MembershipTypeId = customer.MembershipTypeId;
+                custmerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
+
         public ActionResult Details(int id)
         {
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
@@ -39,6 +67,21 @@ namespace VideoRentalStore.Controllers
             }
 
             return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes
+            };
+            return View("CustomerForm", viewModel);
         }
     }
 }
--- VideoRentalStore/Models/Customer.cs
+++ VideoRentalStore/Models/Customer.cs
@@ -14,12 +14,14 @@ namespace VideoRentalStore.Models
         [StringLength(255)]
         public string Name { get; set; }
 
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthday { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
 
         public MembershipType MembershipType { get; set; }
 
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
 
