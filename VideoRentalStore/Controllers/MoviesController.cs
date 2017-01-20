f94dd1f2845ca37ad2635ee33fcf2c47259d7849
yimin-du <d.yimin@gmail.com>
2017-01-21 10:20:54 +1030

    implement add&edit movie

--- VideoRentalStore/Controllers/MoviesController.cs
+++ VideoRentalStore/Controllers/MoviesController.cs
@@ -29,9 +29,62 @@ namespace VideoRentalStore.Controllers
         {
 
 
-            var movies = _context.Movies.ToList();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            // get the movie to edit from db
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if(movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            var genres = _context.Genres.ToList();
+            // create movieFormViewModel
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if(movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            } else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.DateReleased = movie.DateReleased;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
 
         public ActionResult Details(int id)
--- VideoRentalStore/Models/Movie.cs
+++ VideoRentalStore/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,15 +10,21 @@ namespace VideoRentalStore.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public Genre Genre { get; set; }
+
+        [Required]
+        [Display(Name="Genre")]
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Display(Name="Release Date")]
         public DateTime DateReleased { get; set; }
 
+        [Display(Name="Number in stock")]
         public byte NumberInStock { get; set; }
     }
 }
