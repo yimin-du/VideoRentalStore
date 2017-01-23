81b8d64e6caf3b91dff14ce2280b0d8607377bdb
yimin-du <d.yimin@gmail.com>
2017-01-23 12:25:26 +1030

    use datatable

---
+++ VideoRentalStore/Controllers/Api/CustomersController.cs
@@ -0,0 +1,101 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using VideoRentalStore.Dtos;
+using VideoRentalStore.Models;
+using System.Data.Entity;
+
+namespace VideoRentalStore.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/customers
+        public IHttpActionResult GetCustomers()
+        {
+            var customerDtos = _context.Customers
+                .Include(c => c.MembershipType)
+                .ToList()
+                .Select(Mapper.Map<Customer, CustomerDto>);
+
+            return Ok(customerDtos);
+        }
+
+        // GET /api/customer/id
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if(customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Mapper.Map<Customer, CustomerDto>(customer));
+        }
+
+
+        // POST /api/customers
+        [System.Web.Mvc.HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            customerDto.Id = customer.Id;
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
+        }
+
+        // PUT /api/customers/1
+        [System.Web.Mvc.HttpPut]
+        public IHttpActionResult UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            Mapper.Map(customerDto, customerInDb);
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+
+        // DELETE /api/customers/1
+        [System.Web.Mvc.HttpDelete]
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- VideoRentalStore/Controllers/CustomersController.cs
+++ VideoRentalStore/Controllers/CustomersController.cs
@@ -25,8 +25,7 @@ namespace VideoRentalStore.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-            return View(customers);
+            return View();
         }
 
         public ActionResult New()
